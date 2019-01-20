5f21dc93117be2356b5335475f6d41043a93b308
Quyen Truong <quyent22@cs.washington.edu>
2019-01-19 20:14:18 -0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class Calculator {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public static void main(String[] args) {
 
-        String exp = "((1.5+2)*3-4)*5 ^4";
-        System.out.println(parseExp(exp));
-        System.out.println(infixToPostFix(parseExp(exp)));
-        System.out.println(postfixExp(exp));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        System.out.println("Welcome to my program - Calculator");
+        System.out.println("Operator: " + ANSI_YELLOW + "+ - * / % ^ ( )" + ANSI_RESET);
+        System.out.println("Example: " + ANSI_CYAN + "5 + (4 - 7) * 9^2" + ANSI_RESET);
+        while (true) {
+            System.out.print("Enter your math expression: ");
+            try {
+                input = bufferedReader.readLine();
+                if (input.equalsIgnoreCase("stop"))
+                    break;
+                System.out.println("PostFix: " + ANSI_GREEN + String.join(" ", infixToPostFix(parseExp(input))) + ANSI_RESET);
+
+                System.out.println("Result: " + ANSI_BLUE + postfixExp(input) + ANSI_RESET);
+                System.out.println("Type " + ANSI_PURPLE + "'stop' " + ANSI_RESET + "to stop");
+            } catch (Exception e) {
+                System.out.println(ANSI_RED + "Your math expression is invalid. Please try again." + ANSI_RESET);
+            }
+
+        }
     }
 
     private static ArrayList<String> parseExp(String exp) {
@@ -108,7 +137,7 @@ public class Calculator {
         return -1;
     }
 
-    public static double postfixExp(String exp) {
+    public static String postfixExp(String exp) {
         Stack<String> stack = new Stack<>();
         for (String s : infixToPostFix(parseExp(exp))) {
             if (isNumber(s)) {
@@ -121,6 +150,16 @@ public class Calculator {
                 stack.push(String.valueOf(result));
             }
         }
-        return Double.valueOf(stack.pop());
+        double result = Double.valueOf(stack.pop());
+        DecimalFormat df;
+        if ((String.valueOf(result).matches("\\d+\\.?\\d{5,}"))) {
+            df = new DecimalFormat("#.######");
+            return df.format(result);
+        }
+        if (String.valueOf(result).matches("\\d+\\.?0$")) {
+            df = new DecimalFormat("#");
+            return df.format(result);
+        }
+        return String.valueOf(result);
     }
 }
