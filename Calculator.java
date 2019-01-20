fb1efd3111f19490644cca865d3050c3d9031be2
Quyen Truong <quyent22@cs.washington.edu>
2019-01-20 12:32:41 -0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -20,23 +20,45 @@ public class Calculator {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String input;
         System.out.println("Welcome to my program - Calculator");
-        System.out.println("Operator: " + ANSI_YELLOW + "+ - * / % ^ ( )" + ANSI_RESET);
-        System.out.println("Example: " + ANSI_CYAN + "5 + (4 - 7) * 9^2" + ANSI_RESET);
+        System.out.println("Operator: " + ANSI_YELLOW + "+ - * / % ^ | ( )" + ANSI_RESET);
+        System.out.println("Note: " + ANSI_GREEN + "Use [] for absolute. Ex: [5-9]" + ANSI_RESET);
+        System.out.println("Ex of input: " + ANSI_CYAN + "5 + 2([4 - 77] - 8)+2^3" + ANSI_RESET);
+        System.out.println("Type: " + ANSI_PURPLE + "ex1, ex2, ex3, ex4" + ANSI_RESET + " to see example");
         while (true) {
             System.out.print("Enter your math expression: ");
             try {
                 input = bufferedReader.readLine();
-                if (input.equalsIgnoreCase("stop"))
+                input = inputSwitch(input);
+                if (input.equals("stop"))
                     break;
-                System.out.println("PostFix: " + ANSI_GREEN + String.join(" ", infixToPostFix(parseExp(input))) + ANSI_RESET);
 
+                ArrayList<String> exp = parseExp(input);
+                String infix = String.join(" ", exp).replaceAll("\\[", "|").replaceAll("]", "|");
+                String postfix = String.join(" ", infixToPostFix(exp));
+                System.out.println("InFix: " + ANSI_PURPLE + infix + ANSI_RESET);
+                System.out.println("PostFix: " + ANSI_GREEN + postfix + ANSI_RESET);
                 System.out.println("Result: " + ANSI_BLUE + postfixExp(input) + ANSI_RESET);
                 System.out.println("Type " + ANSI_PURPLE + "'stop' " + ANSI_RESET + "to stop");
             } catch (Exception e) {
                 System.out.println(ANSI_RED + "Your math expression is invalid. Please try again." + ANSI_RESET);
             }
-
         }
+    }
+
+    private static String inputSwitch(String input) {
+        switch (input.toLowerCase()) {
+            case "stop":
+                return "stop";
+            case "ex1":
+                return "5 + 2([4 - 77] - 8)+2^3";
+            case "ex2":
+                return "3[[4-6]-22]2^3";
+            case "ex3":
+                return "4* (4+2)(3+3) - 5";
+            case "ex4":
+                return "9(3)(2)[3-6]5(2)";
+        }
+        return input;
     }
 
     private static ArrayList<String> parseExp(String exp) {
@@ -44,6 +66,7 @@ public class Calculator {
         ArrayList<String> result = new ArrayList<>();
         StringBuffer number = new StringBuffer();
         char[] chs = exp.toCharArray();
+        boolean abs = false;
         for (int i = 0; i < chs.length; i++) {
             char ch = chs[i];
             if (Character.isDigit(ch) || ch == '.') {
@@ -54,13 +77,20 @@ public class Calculator {
                     number.delete(0, number.length());
                 }
                 // insert * when previous of '(' is ')' or is non operator
-                if (i > 0 && ch == '(' && (chs[i - 1] == ')' || priority(chs[i - 1]) < -1)) {
-                    result.add("*");
+                if (i > 0) {
+                    if ((ch == '[' || ch == '(')
+                            && (chs[i - 1] == ']' || chs[i - 1] == ')' || priority(chs[i - 1]) < -1)
+                    ) {
+                        result.add("*");
+                    }
                 }
+
                 result.add(ch + "");
                 // insert * when after of ')' is '('
-                if (i < chs.length - 1 && ch == ')' && priority(chs[i + 1]) < -1) {
-                    result.add("*");
+                if (i < chs.length - 1) {
+                    if ((ch == ']' || ch == ')') && priority(chs[i + 1]) < -1) {
+                        result.add("*");
+                    }
                 }
             } else {
                 throw new IllegalArgumentException("Invalid Input");
@@ -89,6 +119,8 @@ public class Calculator {
             case '+':
             case '-':
                 return 1;
+            case '[':
+            case ']':
             case ')':
             case '(':
                 return -1;
@@ -114,7 +146,7 @@ public class Calculator {
                     output.add(stack.pop());
                 }
                 stack.push(s);
-            } else if (s.charAt(0) == '(') { // if s is (
+            } else if (s.charAt(0) == '(' || s.charAt(0) == '[') { // if s is (
                 stack.push(s);
             }
             // if s is )
@@ -124,6 +156,13 @@ public class Calculator {
                     output.add(stack.pop());
                 }
                 stack.pop();
+            } else if (s.charAt(0) == ']') {
+                while (!stack.empty() && stack.peek().charAt(0) != '[') {
+                    output.add(stack.pop());
+                }
+                stack.pop();
+                output.add("1");
+                output.add("|");
             }
         }
         // stack.pop the remaining to output
@@ -134,6 +173,8 @@ public class Calculator {
 
     private static double cal(double x, double y, char op) {
         switch (op) {
+            case '|':
+                return Math.abs(x) * Math.abs(y);
             case '^':
                 return Math.pow(x, y);
             case '+':
@@ -152,7 +193,9 @@ public class Calculator {
 
     public static String postfixExp(String exp) {
         Stack<String> stack = new Stack<>();
-        for (String s : infixToPostFix(parseExp(exp))) {
+        ArrayList<String> Exp = parseExp(exp);
+        Exp = infixToPostFix(Exp);
+        for (String s : Exp) {
             if (isNumber(s)) {
                 stack.push(s);
             } else {
@@ -165,17 +208,19 @@ public class Calculator {
         }
         double result = Double.valueOf(stack.pop());
         DecimalFormat df;
+        // -? check zero or one negative
         // \d+ check more than one digit
         // \.? check zero or one dot
         // \d{5,} check more than five digits
-        if ((String.valueOf(result).matches("\\d+\\.?\\d{5,}"))) {
+        if ((String.valueOf(result).matches("-?\\d+\\.?\\d{5,}"))) {
             df = new DecimalFormat("#.######");
             return df.format(result);
         }
+        // -? check zero or one negative
         // \d+ check more than one digit
         // \.? check zero or one dot
         // 0$ the ending is 0
-        if (String.valueOf(result).matches("\\d+\\.?0$")) {
+        if (String.valueOf(result).matches("-?\\d+\\.?0$")) {
             df = new DecimalFormat("#");
             return df.format(result);
         }
